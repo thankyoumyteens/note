341cce8f7c5da6fe17e215f1fbe47079dc4ca25c
zhaosz <iloveyesterday@outlook.com>
2024-02-18 19:34:59 +0800

    '1'

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 import os
-import re
+import subprocess
 from pathlib import Path
 import shutil
 
@@ -15,16 +15,18 @@ def copy_files(source_path, book_name):
     assets_prefix = "assets/books/" + book_name
     if dest_path.exists():
         shutil.rmtree(str(dest_path))
-    for file_path in source_path.glob("**/*"):
+    subprocess.call(["mdbook", "build"], cwd=source_path)
+    book_path = source_path / "book"
+    for file_path in book_path.glob("**/*"):
         if file_path.is_file():
-            simple_path = file_path.relative_to(source_path)
+            simple_path = file_path.relative_to(book_path)
             simple_path = str(simple_path).replace("\\", "/")
             assets_list.append(assets_prefix + "/" + simple_path)
-    shutil.copytree(str(source_path), str(dest_path))
+    shutil.copytree(str(book_path), str(dest_path))
 
 
 # jvm
-jvm_path = dist_path / "programming" / "Java" / "md" / "jvm" / "book"
+jvm_path = dist_path / "programming" / "Java" / "md" / "jvm"
 copy_files(jvm_path, "jvm")
 
 with pubspec_path.open("w", encoding="utf-8") as f:
@@ -37,7 +39,10 @@ with pubspec_path.open("w", encoding="utf-8") as f:
     f.write("dependencies:\n")
     f.write("  flutter:\n")
     f.write("    sdk: flutter\n")
-    f.write("  flutter_markdown: ^0.6.18+3\n")
+    # f.write("  flutter_markdown: ^0.6.18+3\n")
+    f.write("  jaguar: ^3.1.3\n")
+    f.write("  jaguar_flutter_asset: ^3.0.0\n")
+    f.write("  webview_flutter: ^4.7.0\n")
     f.write("  cupertino_icons: ^1.0.2\n")
     f.write("dev_dependencies:\n")
     f.write("  flutter_test:\n")
